e6f5ed9ec7b7af1b0b529f3767fd27ae55531659
André Felipe Dias <andre.dias@pronus.io>
2023-03-12 19:14:35 -0300

    /hello shouldn't be exposed to docs

--- backend/transfer/routers/hello.py
+++ backend/transfer/routers/hello.py
@@ -4,7 +4,7 @@ from loguru import logger
 router = APIRouter()
 
 
-@router.get('/hello')
+@router.get('/hello', include_in_schema=False)
 async def hello_world() -> dict[str, str]:
     logger.info('Hello world!')
     return {'message': 'Hello World'}
