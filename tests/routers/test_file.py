f0aebe180c7960968247c2fd05d65e335e7cd211
André Felipe Dias <andre.dias@pronus.io>
2023-09-17 11:07:41 -0300

    improved tests for file router

--- tests/routers/test_file.py
+++ tests/routers/test_file.py
@@ -1,4 +1,3 @@
-import secrets
 from pathlib import Path
 from typing import Final
 from unittest.mock import patch
@@ -6,32 +5,27 @@ from unittest.mock import patch
 from fastapi import status
 from httpx import AsyncClient
 
-from transfer import config
 from transfer.file_utils import file_exists
 from transfer.resources import scheduler
 
 SIZE_LIMIT: Final[int] = 10
 
 
-@patch.dict(config.__dict__, {'FILE_SIZE_LIMIT': SIZE_LIMIT})
+@patch('transfer.config.FILE_SIZE_LIMIT', SIZE_LIMIT)
 async def test_upload_file_over_size_limit(tmp_path: Path, client: AsyncClient) -> None:
-    filename = tmp_path / 'over_sized.txt'
-    filename.write_text(secrets.token_hex(SIZE_LIMIT + 1))
-    resp = await client.post('/', files={'file': open(str(filename), 'rb')})
+    hello_world = tmp_path / 'over_sized.txt'
+    hello_world.write_text('x' * (SIZE_LIMIT + 1))
+
+    with hello_world.open('rb') as f:
+        resp = await client.post('/', files={'file': f})
     assert resp.status_code == status.HTTP_413_REQUEST_ENTITY_TOO_LARGE
 
-    # try to cheat by setting the Content-Length header
-    token = secrets.token_urlsafe(config.TOKEN_LENGTH)
-    with patch('transfer.file_utils.secrets.token_urlsafe', return_value=token):
+    # try to cheat by forging the Content-Length header
+    with hello_world.open('rb') as f:
         resp = await client.post(
-            '/',
-            files={'file': open(str(filename), 'rb')},
-            headers={'Content-Length': str(SIZE_LIMIT // 2)},
+            '/', files={'file': f}, headers={'Content-Length': str(SIZE_LIMIT // 2)}
         )
     assert resp.status_code == status.HTTP_413_REQUEST_ENTITY_TOO_LARGE
-    path = config.UPLOAD_DIR / token / filename.name
-    assert not path.exists()
-    assert not path.parent.exists()
 
 
 async def test_file_lifecycle(tmp_path: Path, client: AsyncClient) -> None:
@@ -40,21 +34,22 @@ async def test_file_lifecycle(tmp_path: Path, client: AsyncClient) -> None:
     hello_world.write_text('hello world')
 
     # upload the file
-    resp = await client.post('/', files={'file': open(str(hello_world), 'rb')})
+    with hello_world.open('rb') as f:
+        resp = await client.post('/', files={'file': f})
     assert resp.status_code == status.HTTP_201_CREATED
-    link = resp.headers['Location']
-    token, filename = link.split('/')[-2:]
-    assert link == resp.text == f'http://testserver/{token}/{filename}'
+    token, filename = resp.text.split('/')[-2:]
+    assert resp.headers['Location'] == resp.text == f'http://testserver/{token}/{filename}'
     assert file_exists(token, filename)
     job_id = f'{token}/{filename}'
     assert scheduler.get_job(job_id) is not None
 
     # upload the same file but faking a reverse proxy
-    resp = await client.post(
-        '/',
-        files={'file': open(str(hello_world), 'rb')},
-        headers={'X-Forwarded-Proto': 'https', 'X-Forwarded-Host': 'transfer.pronus.io'},
-    )
+    with hello_world.open('rb') as f:
+        resp = await client.post(
+            '/',
+            files={'file': f},
+            headers={'X-Forwarded-Proto': 'https', 'X-Forwarded-Host': 'transfer.pronus.io'},
+        )
     assert resp.status_code == status.HTTP_201_CREATED
     token2, filename2 = resp.text.split('/')[-2:]
     assert token != token2
@@ -64,7 +59,7 @@ async def test_file_lifecycle(tmp_path: Path, client: AsyncClient) -> None:
     )
 
     # get existing file
-    resp = await client.get(link)
+    resp = await client.get(f'/{token}/{filename}')
     assert resp.status_code == status.HTTP_200_OK
     assert resp.text == 'hello world'
     assert scheduler.get_job(job_id) is not None
@@ -76,7 +71,7 @@ async def test_file_lifecycle(tmp_path: Path, client: AsyncClient) -> None:
     assert scheduler.get_job(job_id) is None
 
     # get non-existing file
-    resp = await client.get(link)
+    resp = await client.get(f'/{token}/{filename}')
     assert resp.status_code == status.HTTP_404_NOT_FOUND
 
     # delete non-existing file
