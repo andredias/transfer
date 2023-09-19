b2e3323fccb2ba8bce264903fa8f539a966691ac
André Felipe Dias <andre.dias@pronus.io>
2023-09-19 11:39:46 -0300

    remove references to hello router

--- transfer/main.py
+++ transfer/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 
 from . import config
 from .resources import lifespan
-from .routers import file, hello
+from .routers import file
 
 app = FastAPI(
     title='Transfer',
     debug=config.DEBUG,
     lifespan=lifespan,
 )
-routers = (hello.router, file.router)
+routers = (file.router,)
 for router in routers:
     app.include_router(router)
